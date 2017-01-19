1637cea03730a4a244a4079803b4d533c123157d
EvgeniyLvov <ipodrayni@gmail.com>
2017-01-19 12:18:43 +0300

    Add project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_many :microposts
+
+  validates_presence_of :name #, :on => "", :message => ""
+  validates_presence_of :email #, :on => "", :message => ""
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: VALID_EMAIL_REGEX
 end
