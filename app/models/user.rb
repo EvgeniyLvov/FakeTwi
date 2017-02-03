21e709057f0f4c4d8be095396ddd17997375429d
EvgeniyLvov <ipodrayni@gmail.com>
2017-02-03 22:23:31 +0400

    Add Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,15 @@ class User < ApplicationRecord
 
   has_many :microposts
 
+  has_secure_password
+
   validates_presence_of :name #, :on => "", :message => ""
   validates_presence_of :email #, :on => "", :message => ""
   validates_uniqueness_of :email, case_sensitive: false
-  validates_format_of :email, with: VALID_EMAIL_REGEX
+  validates_format_of :email, with: VALID_EMAIL_REGEX, if: lambda { |o| o.email.present? }
+
+  validates_presence_of :password
+  validates_length_of :password, {minimum: 6, maximum:14}
+  validates_confirmation_of :password, if: lambda { |o| o.password.present? }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114171512) do
+ActiveRecord::Schema.define(version: 20170202124932) do
 
   create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20170114171512) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_foreign_key "microposts", "users"
